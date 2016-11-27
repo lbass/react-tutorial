90c58271b3c208aceefaaae3f83632f936a7b2a1
hejin <hejin@nkia.co.kr>
2016-11-27 18:54:22 +0900

    ES6 변환 작업

---
+++ examples/01-hello-world(ES6)/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {render} from 'react-dom';
+
+class Main extends Component {
+    render() {
+        return (
+            <div>
+                Hello world
+            </div>
+        );
+    }
+}
+
+render(<Main />, document.getElementById('app'));
